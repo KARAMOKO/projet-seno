835211900e0c01973097230935f4a9a510539b97
S-COULIBALY <s.coulibaly@outlook.com>
2017-03-20 20:47:42 +0100

    création des pages commerciale, ajout Partial navbaret modification dans le dossier Asset

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
+
   def home
   end
+
+  def comment_ca_marche
+  end
+
+  def a_propos
+  end
+
+  def blog
+  end
+
+  def nos_engagements
+  end
+
+  def join_us
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "/comment_ca_marche"  => "pages#comment_ca_marche"
+  get "/a_propos"  => "pages#a_propos"
+  get "/blog"  => "pages#blog"
+  get "/nos_engagements"  => "pages#nos_engagements"
+  get "/join_us"  => "pages#join_us"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
